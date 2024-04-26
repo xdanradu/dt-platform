6b6edf2fe8425c7fade3107dbabb43cf149f2398
Dan Radu <xdanradu@yahoo.com>
2024-04-26 15:48:26 +0300

    cleanup

--- frontend/control/movies-component.js
+++ frontend/control/movies-component.js
@@ -3,14 +3,15 @@ new Vue(
         el: '#movies-component',
         data: {
             movies: [],
-            resourcesApi: 'http://localhost:3200'
+            resourcesApi: 'http://localhost:55970',
+            httpBackendUrl: 'http://localhost:3000'
         },
         created: function(){
             this.getMovies();
         },
         methods: {
             getMovies: function(){
-                axios.get('http://localhost:3000/movies').then(response => { 
+                axios.get(`${this.httpBackendUrl}/movies`).then(response => { 
                     this.movies = response.data;
                 });
             }
